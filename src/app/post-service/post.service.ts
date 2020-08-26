b14d6a4ec6d984a02a29f9a349256515538b19bc
gummangummangumman <trygve@igesund.com>
2020-08-26 16:00:35 +0200

    Add character cap to tweets with error message

--- src/app/post-service/post.service.ts
+++ src/app/post-service/post.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class PostService {
 
-  text = "<Default tweet>";
+  text = "";
 
   constructor() {
   }
--- src/app/tweet/tweet.component.ts
+++ src/app/tweet/tweet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post-service/post.service';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'tweet',
@@ -9,9 +10,22 @@ import { PostService } from '../post-service/post.service';
 export class TweetComponent implements OnInit {
 
   postService:PostService;
+  maxNumberOfCharacters = 280;
 
   constructor(postService:PostService) {
     this.postService = postService;
+  }
+
+  //returns the first i characters of a string
+  first(text:string, i:number = this.maxNumberOfCharacters)
+  {
+    return text.slice(0, i);
+  }
+
+  //returns the characters from i to the end of a string
+  rest(text:string, i:number = this.maxNumberOfCharacters)
+  {
+    return text.slice(i);
   }
 
   ngOnInit(): void {
