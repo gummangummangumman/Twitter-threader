7e742bf9a99b10d4e5beb9d0630bc8feb8409d29
gummangummangumman <trygve@igesund.com>
2020-09-01 10:32:08 +0200

    Show tweet thread as multiple tweets

--- src/app/tweet-thread/tweet-thread.component.ts
+++ src/app/tweet-thread/tweet-thread.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges, Input } from '@angular/core';
 import { PostService } from '../post-service/post.service';
 
 @Component({
@@ -8,13 +8,42 @@ import { PostService } from '../post-service/post.service';
 })
 export class TweetThreadComponent implements OnInit {
 
+  @Input() post:string;
+  maxNumberOfCharacters:number = 280;
+  tweetPosts:string[];
   postService:PostService;
+
   constructor(postService:PostService)
   {
     this.postService = postService;
+    this.tweetPosts = [""];
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges)
+  {
+    let text = this.post;
+    this.tweetPosts = [];
+    while (text !== "")
+    {
+      this.tweetPosts.push(this.first(text));
+      text = this.rest(text);
+    }
+    console.log(this.tweetPosts);
+  }
+
+  //returns the first i characters of a string
+  first(text:string, i:number = this.maxNumberOfCharacters)
+  {
+    return text.slice(0, i);
+  }
+
+  //returns the characters from i to the end of a string
+  rest(text:string, i:number = this.maxNumberOfCharacters)
+  {
+    return text.slice(i);
   }
 
 }
